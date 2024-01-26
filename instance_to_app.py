68ef384693e1ba9aef3b530b783de1547845bb34
David Lohle <2215601+davidlohle@users.noreply.github.com>
2024-01-26 11:02:36 -0500

    Ignore crashed processes, as you can't grab their name/guid/etc.

--- instance_to_app.py
+++ instance_to_app.py
@@ -13,6 +13,7 @@ if os.environ.get("BOSH_ENVIRONMENT") is None:
     print("You need to set the BOSH director environment variables such as 'BOSH_ENVIRONMENT', 'BOSH_CLIENT', 'BOSH_CLIENT_SECRET', and 'BOSH_CA_CERT'")
     sys.exit(1)
 
+#subprocess.run(["bosh", "deps", "--json"], capture_output=True)
 
 # Get the cfdot output from a Diego cell. This rube goldberg-esque approach is required because Diego doesn't expose this data freely,
 # and cfdot is the only way we can (easily) get access to it.
@@ -29,8 +30,12 @@ mapped_apps = ["instance_guid,app_guid,app_name,org_name,space_name"]
 with open("/tmp/cfdot-output.json", 'r') as lrp_data_raw:
     lrp_data = json.load(lrp_data_raw)
     for app in lrp_data:
-        mapped_apps.append(
-            f"{app['instance_guid']},{app['metric_tags']['app_id']},{app['metric_tags']['app_name']},{app['metric_tags']['organization_name']},{app['metric_tags']['space_name']}")
+        if app['state'] is "RUNNING":
+            mapped_apps.append(
+                f"{app['instance_guid']},{app['metric_tags']['app_id']},{app['metric_tags']['app_name']},{app['metric_tags']['organization_name']},{app['metric_tags']['space_name']}")
+        else:
+            print(f"App with process GUID {app['process_guid']} is crashed; cannot get information. Skipping.")
+
 # Write to disk
 with open(f"instance_guid_report_{time.strftime('%Y-%m-%d_at_%H-%M-%S')}.csv", 'w') as csv_file:
     for app in mapped_apps:
