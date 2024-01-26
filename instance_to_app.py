036b70657d46d62fdf792b8ddb763f6daf8a8a19
David Lohle <2215601+davidlohle@users.noreply.github.com>
2024-01-25 23:17:05 -0500

    Instance GUID to App GUID

---
+++ instance_to_app.py
@@ -0,0 +1,40 @@
+import subprocess
+import os
+import sys
+import json
+import time
+
+# We need the BOSH deployment name for TAS/CF because we're going to SSH into a Diego cell later on
+if os.environ.get("CF_DEPLOYMENT") is None:
+    print("You need to set the environment variable 'CF_DEPLOYMENT' to the BOSH deployment name of CF.")
+    sys.exit(1)
+
+if os.environ.get("BOSH_ENVIRONMENT") is None:
+    print("You need to set the BOSH director environment variables such as 'BOSH_ENVIRONMENT', 'BOSH_CLIENT', 'BOSH_CLIENT_SECRET', and 'BOSH_CA_CERT'")
+    sys.exit(1)
+
+
+# Get the cfdot output from a Diego cell. This rube goldberg-esque approach is required because Diego doesn't expose this data freely,
+# and cfdot is the only way we can (easily) get access to it.
+print("Grabbing LRP data from BBS...")
+subprocess.run(["bosh", "ssh", "-d", f"{os.environ.get('CF_DEPLOYMENT')}", "diego_cell/0",
+               "-c", "source /var/vcap/jobs/cfdot/bin/setup && cfdot actual-lrps | jq -s > /tmp/cfdot-output.json"])
+subprocess.run(["bosh", "scp", "-d", f"{os.environ.get('CF_DEPLOYMENT')}",
+               "diego_cell/0:/tmp/cfdot-output.json", "/tmp/cfdot-output.json"])
+
+# Create a CSV file with all the data we need to import into whatever Excel thingamabob we want to use.
+print("Collating data...")
+mapped_apps = ["instance_guid,app_guid,app_name,org_name,space_name"]
+# Parse data from BBS
+with open("/tmp/cfdot-output.json", 'r') as lrp_data_raw:
+    lrp_data = json.load(lrp_data_raw)
+    for app in lrp_data:
+        mapped_apps.append(
+            f"{app['instance_guid']},{app['metric_tags']['app_id']},{app['metric_tags']['app_name']},{app['metric_tags']['organization_name']},{app['metric_tags']['space_name']}")
+# Write to disk
+with open(f"instance_guid_report_{time.strftime('%Y-%m-%d_at_%H-%M-%S')}.csv", 'w') as csv_file:
+    for app in mapped_apps:
+        csv_file.write(f"{app}\n")
+print("Complete.")
+
+# How I feel right now: https://www.youtube.com/watch?v=y8OnoxKotPQ
